c32674928b188c831ccb3c7e82d05d77c90ca935
Fallen032 <94450839+Fallen032@users.noreply.github.com>
2023-09-27 07:20:11 -0700

    Delete AddArray.java

--- AddArray.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class AddArray(int arr) {
-   Scanner input = new Scanner(System.in);
-   
-   System.out.print("Enter the size of the Array ----> ");
-   arr = input.nextInt();
-}
