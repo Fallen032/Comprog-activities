48af5eb68b9949dabb1b5a79c96c15d3fd812edf
Gaylord Makahorny <94450839+Fallen032@users.noreply.github.com>
2023-09-27 03:02:53 +0100

    Add files via upload

---
+++ AddArray.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class AddArray(int arr) {
+   Scanner input = new Scanner(System.in);
+   
+   System.out.print("Enter the size of the Array ----> ");
+   arr = input.nextInt();
+}
